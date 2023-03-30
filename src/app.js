b38ecbbacae9380e851aff174808362c9091f403
NjokiKimani <maninjoki@gmail.com>
2023-03-29 21:16:34 -0700

    Added dropdown menu

--- src/app.js
+++ src/app.js
@@ -1,19 +1,8 @@
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function showDropdown() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function (event) {
-  if (!event.target.matches(".menu")) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains("show")) {
-        openDropdown.classList.remove("show");
+   function showDropdown() {
+      var click = document.getElementsByClassName("dropdown-content");
+      if (click.style.display === "none") {
+         click.style.display = "block";
+      } else {
+         click.style.display = "none";
       }
-    }
-  }
-};
+   }
